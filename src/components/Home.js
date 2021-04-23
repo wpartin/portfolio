776c23201f545820ea955bea2e0732ae8cad5f34
Weston Partin <djwest91@gmail.com>
2021-04-23 13:58:00 -0500

    home 1.0

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,21 @@
 import React from "react";
+import image from "../bgimg.jpg";
 
 export default function Home() {
-  return <h1>Home Page!</h1>;
+  return (
+    <main>
+      <img
+        src={image}
+        alt="mountain road"
+        className="absolute objec-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-blue-100 font-bold poppins leading-none lg:leading-snug home-name">
+          Hey there,
+          <br />
+          I'm Weston.
+        </h1>
+      </section>
+    </main>
+  );
 }
