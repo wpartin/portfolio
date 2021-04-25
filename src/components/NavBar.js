9de6fef8c78d75569279f687c553310c938625ea
Weston Partin <djwest91@gmail.com>
2021-04-25 09:11:33 -0500

    small screens adjustments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,39 +5,43 @@ import { SocialIcon } from "react-social-icons";
 export default function NavBar() {
   return (
     <header className="bg-blue-900">
-      <div className="container mx-auto flex-col md:flex-row justify-between">
-        <nav className="flex-col md:flex-row w-screen md:pl-24">
-          <NavLink
-            to="/"
-            exact
-            activeClassName="text-yellow-400"
-            className="inline-flex items-center text-white py-6 px-3 mr-4 hover:text-yellow-400 text-4xl font-bold poppins tracking-widest"
-          >
-            Weston
-          </NavLink>
-          <NavLink
-            to="/post"
-            activeClassName="text-blue-100 bg-blue-700"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-yellow-400"
-          >
-            Blog Posts
-          </NavLink>
-          <NavLink
-            to="/project"
-            activeClassName="text-blue-100 bg-blue-700"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-yellow-400"
-          >
-            Projects
-          </NavLink>
-          <NavLink
-            to="/about"
-            activeClassName="text-blue-100 bg-blue-700"
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-yellow-400"
-          >
-            About Me
-          </NavLink>
+      <div className="container mx-auto flex flex-col md:flex-row justify-between">
+        <nav className="flex flex-col items-center md:items-left md:flex-row w-screen md:pl-24">
+          <div className="flex">
+            <NavLink
+              to="/"
+              exact
+              activeClassName="text-yellow-400"
+              className="inline-flex items-center text-white pt-6 md:py-6 px-3 mr-4 hover:text-yellow-400 text-4xl font-bold poppins tracking-widest"
+            >
+              Weston
+            </NavLink>
+          </div>
+          <div className="flex">
+            <NavLink
+              to="/post"
+              activeClassName="text-blue-100 bg-blue-700"
+              className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-yellow-400"
+            >
+              Blog Posts
+            </NavLink>
+            <NavLink
+              to="/project"
+              activeClassName="text-blue-100 bg-blue-700"
+              className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-yellow-400"
+            >
+              Projects
+            </NavLink>
+            <NavLink
+              to="/about"
+              activeClassName="text-blue-100 bg-blue-700"
+              className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-yellow-400"
+            >
+              About Me
+            </NavLink>
+          </div>
         </nav>
-        <div className="inline-flex py-3 px-3 my-6 pr-24">
+        <div className="inline-flex justify-center py-3 px-3 mb-3 md:my-6 md:pr-24">
           <SocialIcon
             url="http://linkedin.com/"
             className="mr-4"
