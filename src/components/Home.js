1b72a00a8a7bfe5190fba9d6a9fe4152eccda7ab
Weston Partin <djwest91@gmail.com>
2021-05-01 07:47:38 -0500

    changed h-full to h-screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
       <img
         src={image}
         alt="mountain road"
-        className="absolute object-cover w-full h-full"
+        className="absolute object-cover w-full h-screen"
       />
       <section className="relative flex justify-center min-h-screen pt-12 md:pt-38 lg:pt-64 px-8">
         <h1 className="text-6xl md:text-8xl text-blue-100 font-bold poppins leading-none lg:leading-snug">
