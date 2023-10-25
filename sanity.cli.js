589a0f93df9f690b85b866e5a2ecd04aeb4ec4db
wpartin <djwest91@gmail.com>
2023-10-25 13:07:39 -0500

    [v2.0.1]

--- sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: '22eiy0sq',
-    dataset: 'production'
-  }
-})
--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import schemas from "./schemas/schema";
-import deskStructure from "./deskStructure";
 
 export default defineConfig({
     title: "blog",
     projectId: "22eiy0sq",
     dataset: "production",
-    plugins: [deskTool({
-        structure: deskStructure
-    })],
+    plugins: [deskTool()],
     schema: {
         types: schemas,
     },
