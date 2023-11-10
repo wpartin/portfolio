65b75ab56f6485a7d027b31f469b15a63b833b0a
wpartin <djwest91@gmail.com>
2023-11-10 10:57:58 -0600

    [master] more inputs for code block

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import { codeInput } from "@sanity/code-input";
 import schemas from "./schemas/schema";
 
 export default defineConfig({
-    title: "blog",
-    projectId: "22eiy0sq",
-    dataset: "production",
-    plugins: [deskTool()],
-    schema: {
-        types: schemas,
-    },
+  title: "blog",
+  projectId: "22eiy0sq",
+  dataset: "production",
+  plugins: [codeInput(), deskTool()],
+  schema: {
+    types: schemas,
+  },
 });
--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -3,7 +3,7 @@ import blockContent from "./blockContent";
 import post from "./post";
 import author from "./author";
 import project from "./project";
-import code from "@sanity/code-input";
+import code from "./code";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default [post, author, project, blockContent, code];
