a5b2185ca3dc7411687431a97b7352944ecb97fe
wpartin <djwest91@gmail.com>
2023-11-10 12:51:18 -0600

    [master]

--- src/components/serializers.js
+++ src/components/serializers.js
@@ -1,14 +1,10 @@
 import { solarizedDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
-export const serializers = {
-  types: {
-    code: (props) => (
-      <div className="my-2">
-        <SyntaxHighlighter language={props.go.language} style={solarizedDark}>
-          {props.go.code}
-        </SyntaxHighlighter>
-      </div>
-    ),
-  },
+export const serializera = ({ node }) => {
+  return (
+    <SyntaxHighlighter language={node.language || "text"} style={solarizedDark}>
+      {node.code}
+    </SyntaxHighlighter>
+  );
 };
