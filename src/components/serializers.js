866af0ebb25d4919a4866070765fd70ec754c48f
wpartin <djwest91@gmail.com>
2023-11-10 12:45:56 -0600

    [master]

--- src/components/serializers.js
+++ src/components/serializers.js
@@ -1,13 +1,14 @@
-const CodeBlockSerializer = ({ node }) => {
-  return (
-    <pre>
-      <code>{node.code}</code>;
-    </pre>
-  );
-};
+import { solarizedDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { SyntaxHighlighter } from "react-syntax-syntax-highlighter";
 
 export const serializers = {
   types: {
-    code: CodeBlockSerializer,
+    code: (props) => (
+      <div className="my-2">
+        <SyntaxHighlighter language={props.go.language} style={solarizedDark}>
+          {props.go.code}
+        </SyntaxHighlighter>
+      </div>
+    ),
   },
 };
