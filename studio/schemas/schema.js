a8773bb1e5fc7cb343a3ddb0ebd4ba1ba630246f
wpartin <djwest91@gmail.com>
2023-11-10 10:42:42 -0600

    [master] proper code import

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -3,7 +3,7 @@ import blockContent from "./blockContent";
 import post from "./post";
 import author from "./author";
 import project from "./project";
-import code from "./code";
+import code from "@sanity/code-input";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default [post, author, project, blockContent, code];
