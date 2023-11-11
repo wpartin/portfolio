6ac883bf2a1ef51876391277510e0531fda98a73
wpartin <djwest91@gmail.com>
2023-11-11 06:55:30 -0600

    [master] update to code serializer

--- src/components/serializers.js
+++ src/components/serializers.js
@@ -1,10 +1,16 @@
 import { solarizedDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
-export const serializers = ({ node }) => {
+const CodeBlockSerializer = ({ node }) => {
   return (
     <SyntaxHighlighter language={node.language || "text"} style={solarizedDark}>
       {node.code}
     </SyntaxHighlighter>
   );
+};
+
+export const serializers = {
+  types: {
+    code: CodeBlockSerializer,
+  },
 };
