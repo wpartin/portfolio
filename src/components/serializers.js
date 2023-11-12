87911c6acaea77ea3a62c43a0c12dbeb0fb7341b
wpartin <djwest91@gmail.com>
2023-11-12 07:04:18 -0600

    [master]

--- src/components/serializers.js
+++ src/components/serializers.js
@@ -1,10 +1,10 @@
-import { solarizeddark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { solarizedDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
 const CodeBlockSerializer = ({ props }) => {
   const { code, language } = props;
   return (
-    <SyntaxHighlighter language={language} style={solarizeddark}>
+    <SyntaxHighlighter language={language} style={solarizedDark}>
       {code}
     </SyntaxHighlighter>
   );
