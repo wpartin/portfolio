abe8c6d9fc3920413160479f7cee969f61681848
wpartin <djwest91@gmail.com>
2023-11-13 15:42:04 -0600

    [master] rm unneeded 'code' const

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -11,25 +11,6 @@ function urlFor(source) {
   return builder.image(source);
 }
 
-const code = `
-package main
-
-import (
-    "fmt"
-    "os"
-)
-
-func main () {
-    var s, sep string
-
-    for i := 0; i < len(os.Args); i++ {
-        s += sep + os.Args[i]
-        sep = " "
-    }
-    fmt.Println(s)
-}
-`;
-
 export default function SinglePost() {
   const [singlePost, setSinglePost] = useState(null);
   const { slug } = useParams();
