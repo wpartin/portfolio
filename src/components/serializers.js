997b41ce0a20feafc2e284ee07ed221a53b7c3c6
wpartin <djwest91@gmail.com>
2023-11-13 12:52:14 -0600

    [master]

--- src/components/serializers.js
+++ src/components/serializers.js
@@ -1,20 +1,15 @@
-import { solarizedDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { golang } from "react-syntax-highlighter/dist/esm/languages/prism";
+import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
-SyntaxHighlighter.registerLanguage("golang", golang);
-
-const CodeBlockSerializer = ({ node }) => {
-  let language = node.language || "text";
+const serializers = ({ props }) => {
   return (
-    <SyntaxHighlighter language={language} style={solarizedDark}>
-      {node.code}
+    <SyntaxHighlighter
+      language={props.node.language || "text"}
+      style={a11yDark}
+    >
+      {props.node.code}
     </SyntaxHighlighter>
   );
 };
 
-export const serializers = {
-  types: {
-    code: CodeBlockSerializer,
-  },
-};
+export default serializers;
