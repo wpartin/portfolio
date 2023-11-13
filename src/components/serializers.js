cdf087feadde41a11232d680209d9e18847db997
wpartin <djwest91@gmail.com>
2023-11-13 11:39:50 -0600

    [master] still working on styling

--- src/components/serializers.js
+++ src/components/serializers.js
@@ -1,5 +1,8 @@
 import { solarizedDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { golang } from "react-syntax-highlighter/dist/esm/languages/prism";
+
+SyntaxHighlighter.registerLanguage("golang", golang);
 
 const CodeBlockSerializer = ({ node }) => {
   let language = node.language || "text";
