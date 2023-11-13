6ca997e628a8da613a787ab71db2259575eabfd0
wpartin <djwest91@gmail.com>
2023-11-13 09:54:21 -0600

    [master] didn't like the style

--- src/components/serializers.js
+++ src/components/serializers.js
@@ -1,10 +1,10 @@
-import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { solarizedDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 
 const CodeBlockSerializer = ({ node }) => {
   let language = node.language || "text";
   return (
-    <SyntaxHighlighter language={language} style={dark}>
+    <SyntaxHighlighter language={language} style={solarizedDark}>
       {node.code}
     </SyntaxHighlighter>
   );
